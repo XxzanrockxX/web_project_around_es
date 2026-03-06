c91c7e603ecf4008667e2e7111393312ca4aa56c
Luis Philco <luisenin@hotmail.com>
2026-03-06 15:33:03 -0500

    Implement form validation for profile and card creation modals with error handling

--- scripts/index.js
+++ scripts/index.js
@@ -95,6 +95,16 @@ function fillProfileForm() {
  */
 function handleOpenEditModal() {
   fillProfileForm();
+
+  const inputList = Array.from(editForm.querySelectorAll('.popup__input'));
+  const buttonElement = editForm.querySelector('.popup__button');
+
+  inputList.forEach((inputElement) => {
+    hideInputError(editForm, inputElement);
+  });
+
+  toggleButtonState(inputList, buttonElement);
+
   openModal(editPopup);
 }
 
@@ -109,6 +119,56 @@ function handleProfileFormSubmit(evt) {
   profileDescription.textContent = jobInput.value;
   closeModal(editPopup);
 }
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__error_visible');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__error_visible');
+  errorElement.textContent = '';
+}
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__button_disabled');
+    buttonElement.disabled = true;
+  } else {
+    buttonElement.classList.remove('popup__button_disabled');
+    buttonElement.disabled = false;
+  }
+};
+
+const setEventListenersForValidation = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__button');
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);  
+    });
+  });
+};
+
 
 /**
  * Crea un nuevo elemento de tarjeta a partir de los datos proporcionados.
@@ -192,6 +252,9 @@ addCardButton.addEventListener('click', () => {
 addCardForm.addEventListener('submit', handleAddCardFormSubmit);
 editButton.addEventListener('click', handleOpenEditModal);
 editForm.addEventListener('submit', handleProfileFormSubmit);
+
+setEventListenersForValidation(editForm);
+setEventListenersForValidation(addCardForm);
 
 
 
