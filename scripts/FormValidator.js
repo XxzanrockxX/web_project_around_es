32fd1e5fec0669418efa384d5c588ecb1ab3e305
Luis Philco <luisenin@hotmail.com>
2026-03-23 16:28:07 -0500

    Add error handling methods to FormValidator class for improved form validation

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -7,6 +7,20 @@ class FormValidator {
         this._buttonElement = this._formElement.querySelector(this._settings.submitButtonSelector);
 
     }
+
+    _showInputError(inputElement, errorMessage) {
+        const errorElement = this._formElement.querySelector(`#${inputElement.id}-error`);
+        inputElement.classList.add(this._settings.inputErrorClass);
+        errorElement.textContent = errorMessage;
+        errorElement.classList.add(this._settings.errorClass);
+}
+
+    _hideInputError(inputElement){
+        const errorElement = this._formElement.querySelector(`#${inputElement.id}-error`);
+        inputElement.classList.remove(this._settings.inputErrorClass);
+        errorElement.classList.remove(this._settings.errorClass);
+        errorElement.textContent = "";
+}
 }
 
  export default FormValidator;
